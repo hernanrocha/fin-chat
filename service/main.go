99802a263c1a39dd262d9ad1f6998e372207f68a
Hernan Rocha <hernanrocha93@gmail.com>
2019-09-18 18:10:49 -0300

    Remove rabbit connection

--- service/main.go
+++ service/main.go
@@ -6,13 +6,10 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/streadway/amqp"
 
-	"github.com/hernanrocha/fin-chat/messenger"
 	"github.com/hernanrocha/fin-chat/service/controller"
 	_ "github.com/hernanrocha/fin-chat/service/docs"
 	"github.com/hernanrocha/fin-chat/service/hub"
-	"github.com/hernanrocha/fin-chat/service/hub/handler"
 	"github.com/hernanrocha/fin-chat/service/models"
 )
 
@@ -55,30 +52,34 @@ func main() {
 	// Run migration
 	models.Setup(db)
 
-	// Setup RabbitMQ
-	rabbitconn := getEnv("RABBIT_CONNECTION", "amqp://rabbitmq:rabbitmq@localhost:5672/")
-	conn, err := amqp.Dial(rabbitconn)
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	/*
+		// Setup RabbitMQ
+		rabbitconn := getEnv("RABBIT_CONNECTION", "amqp://rabbitmq:rabbitmq@localhost:5672/")
+		conn, err := amqp.Dial(rabbitconn)
+		failOnError(err, "Failed to connect to RabbitMQ")
+		defer conn.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+		ch, err := conn.Channel()
+		failOnError(err, "Failed to open a channel")
+		defer ch.Close()
 
-	msg := messenger.NewRabbitMessenger(ch)
-	failOnError(err, "Failed to declare a queue")
+		msg := messenger.NewRabbitMessenger(ch)
+		failOnError(err, "Failed to declare a queue")
+	*/
 
 	// Run Messages Hub
 	h := hub.NewHub()
 	h.Run()
 
-	// Add CmdMessageHandler
-	handler, err := handler.NewCmdMessageHandler("cmd-rabbit", msg, h, models.GetDB())
-	failOnError(err, "Error starting command message handler")
-	h.AddClient(handler)
+	/*
+		// Add CmdMessageHandler
+		handler, err := handler.NewCmdMessageHandler("cmd-rabbit", msg, h, models.GetDB())
+		failOnError(err, "Error starting command message handler")
+		h.AddClient(handler)
 
-	// Run CmdResponse Consumer
-	go msg.StartConsumer(handler.CmdResponseHandler)
+		// Run CmdResponse Consumer
+		go msg.StartConsumer(handler.CmdResponseHandler)
+	*/
 
 	// Setup router
 	r := controller.SetupRouter(h)
